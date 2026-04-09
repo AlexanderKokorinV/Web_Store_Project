c23224f0852232753ba7367b709ca333849b5f78
Alexander <sanya.core@gmail.com>
2026-04-09 11:56:13 +0300

    README.md has been added

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,10 +1,10 @@
-from django.urls import path, include
+from django.urls import path
 from catalog.apps import CatalogConfig
+from catalog.views import home, contacts
 
 APP_NAME = CatalogConfig.name
 
 urlpatterns = [
-    path("",
-
-         ),
+    path("", home, name="home"),
+    path("contacts/", contacts, name="contacts"),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.contrib import messages
+from django.http import HttpResponse
 
-# Create your views here.
+
+def home(request):
+    return render(request, "catalog/home.html")
+
+
+def contacts(request):
+    if request.method == "POST":
+        # Получаем данные из полей
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        message = request.POST.get("message")
+
+        # Добавляем сообщение об успехе
+        return HttpResponse(f"Спасибо, {name}! Ваше сообщение получено.")
+
+    return render(request, "catalog/contacts.html")
