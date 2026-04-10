35e7529e33bac7a633c226f044124e1ade55ee30
Alexander <sanya.core@gmail.com>
2026-04-10 16:46:57 +0300

    migrations have been created

--- catalog/models.py
+++ catalog/models.py
@@ -5,7 +5,7 @@ class Product(models.Model):
     product_name = models.CharField(max_length=150, verbose_name="Наименование продукта", help_text="Введите наименование продукта")
     product_description = models.TextField(verbose_name="Описание продукта", help_text="Введите описание продукта", blank=True, null=True)
     image = models.ImageField(upload_to="catalog/image", blank=True, null=True, verbose_name="Изображение", help_text="Загрузите изображение")
-    category_name = models.ForeignKey(on_delete=models.CASCADE, verbose_name="Категория", related_name="products")
+    category_name = models.ForeignKey("Category", on_delete=models.CASCADE, verbose_name="Категория", related_name="products")
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Цена за покупку")
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
     updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата последнего изменения")
