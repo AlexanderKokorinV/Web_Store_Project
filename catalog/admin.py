6b89b93a7a4700b5bcae7989149bb1b7f8cda17c
Alexander <sanya.core@gmail.com>
2026-04-10 22:42:04 +0300

    views module has been updated

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from catalog.models import Product, Category
+from catalog.models import Product, Category, Contact
 
 
 @admin.register(Product)
@@ -11,3 +11,7 @@ class ProductAdmin(admin.ModelAdmin):
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ("id", "category_name",)
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ("country", "inn", "address",)
--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.contrib import messages
 from django.http import HttpResponse, HttpRequest
-from .models import Product
+from .models import Product, Contact
 
 
 def home(request: HttpRequest) -> HttpResponse:
@@ -26,13 +26,18 @@ def contacts(request: HttpRequest) -> HttpResponse:
     При GET-запросе возвращает страницу с формой обратной связи.
     При POST-запросе принимает данные формы и возвращает сообщение об успехе.
     """
+
+    # Получаем данные из базы
+    contact_data = Contact.objects.first()
+
+
     if request.method == "POST":
         # Получаем данные из полей
         name = request.POST.get("name")
         email = request.POST.get("email")
         message = request.POST.get("message")
 
-        # Добавляем сообщение об успехе
-        return HttpResponse(f"Спасибо, {name}! Ваше сообщение получено.")
+        # Добавляем сообщение
+        print(f"Сообщение от {name} ({email}): {message}")
 
-    return render(request, "catalog/contacts.html")
+    return render(request, "catalog/contacts.html", {"contact": contact_data})
