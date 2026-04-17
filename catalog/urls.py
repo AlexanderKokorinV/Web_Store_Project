04a2cecb67c29599c547dd1e7a51f773fec67cb9
Alexander <sanya.core@gmail.com>
2026-04-17 11:27:39 +0300

    blog app has been created

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 
 from catalog.apps import CatalogConfig
-from catalog.views import add_product, contacts, home, product_detail
+from catalog.views import ProductListView, ProductDetailView, ContactsTemplateView, ProductCreateView
 
 APP_NAME = CatalogConfig.name
 
 urlpatterns = [
-    path("", home, name="home"),
-    path("contacts/", contacts, name="contacts"),
-    path("product/<int:pk>", product_detail, name="product_detail"),
-    path("product/add/", add_product, name="add_product"),
+    path("", ProductListView.as_view(), name="home"),
+    path("product/<int:pk>", ProductDetailView.as_view(), name="product_detail"),
+    path("contacts/", ContactsTemplateView.as_view(), name="contacts"),
+    path("product/add/", ProductCreateView.as_view(), name="add_product"),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,73 +1,121 @@
-from django.core.paginator import Paginator
-from django.http import HttpRequest, HttpResponse
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect
+from django.views.generic import ListView, DetailView, CreateView, TemplateView
+from django.urls import reverse_lazy
 
 from .forms import ProductForm
 from .models import Contact, Product
 
 
-def home(request: HttpRequest) -> HttpResponse:
-    """Отображает главную страницу каталога с навигацией."""
-    # Все товары, отсортированные по дате создания
-    product_list = Product.objects.all().order_by("-created_at")
-
-    # Настройка пагинации (по 3 товара на страницу)
-    paginator = Paginator(product_list, 3)
-
-    # Получение номера текущей страницы из URL
-    page_number = request.GET.get("page")
-
-    # Получение объекта страницы
-    page_obj = paginator.get_page(page_number)
-
-    # Формирование контекста
-    context = {
-        "page_obj": page_obj,
-    }
-
-    return render(request, "catalog/home.html", context)
+class ProductListView(ListView):
+    model = Product
+    template_name = "catalog/home.html" # Путь к шаблону
+    context_object_name = "product_list"
+    paginate_by = 3
+    ordering = ["-created_at"]
 
 
-def contacts(request: HttpRequest) -> HttpResponse:
-    """
-    Обрабатывает страницу контактов.
-    При GET-запросе возвращает страницу с формой обратной связи.
-    При POST-запросе принимает данные формы и возвращает сообщение об успехе.
-    """
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = "catalog/product_detail.html"
 
-    # Получаем данные из базы
-    contact_data = Contact.objects.first()
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = f"Купить {self.object.product_name}"
+        return context
 
-    if request.method == "POST":
-        # Получаем данные из полей
+
+class ProductCreateView(CreateView):
+    model = Product
+    form_class = ProductForm
+    template_name = "catalog/product_form.html"
+
+    success_url = reverse_lazy("catalog:home")
+
+
+class ContactsTemplateView(TemplateView):
+    template_name = "catalog/contacts.html"
+    success_url = reverse_lazy("catalog:contacts")
+
+    def get_context_data(self, **kwargs):
+        """Передает данные о контактах из базы в шаблон"""
+        context = super().get_context_data(**kwargs)
+        context["contact"] = Contact.objects.first()
+        return context
+
+    def post(self, request, *args, **kwargs):
+        """"Обработка POST-запроса (данных из формы)"""
         name = request.POST.get("name")
         email = request.POST.get("email")
         message = request.POST.get("message")
 
-        # Добавляем сообщение
-        print(f"Сообщение от {name} ({email}): {message}")
+        print(f"Сообщение от {name}, {email}: {message}")
 
-    return render(request, "catalog/contacts.html", {"contact": contact_data})
+        return redirect(self.success_url)
+
+# Ниже в комментариях код с использованием FBV:
+
+# def home(request: HttpRequest) -> HttpResponse:
+#     """Отображает главную страницу каталога с навигацией."""
+#     # Все товары, отсортированные по дате создания
+#     product_list = Product.objects.all().order_by("-created_at")
+#
+#     # Настройка пагинации (по 3 товара на страницу)
+#     paginator = Paginator(product_list, 3)
+#
+#     # Получение номера текущей страницы из URL
+#     page_number = request.GET.get("page")
+#
+#     # Получение объекта страницы
+#     page_obj = paginator.get_page(page_number)
+#
+#     # Формирование контекста
+#     context = {
+#         "page_obj": page_obj,
+#     }
+#
+#     return render(request, "catalog/home.html", context)
 
 
-def product_detail(request: HttpRequest, pk: int) -> HttpResponse:
-    """Отображает детальную информацию о конкретном товаре."""
-    product = Product.objects.get(pk=pk)
-    context = {"object": product, "title": f"Купить {product.product_name}"}
-    return render(request, "catalog/product_detail.html", context)
+# def contacts(request: HttpRequest) -> HttpResponse:
+#     """
+#     Обрабатывает страницу контактов.
+#     При GET-запросе возвращает страницу с формой обратной связи.
+#     При POST-запросе принимает данные формы и возвращает сообщение об успехе.
+#     """
+#
+#     # Получаем данные из базы
+#     contact_data = Contact.objects.first()
+#
+#     if request.method == "POST":
+#         # Получаем данные из полей
+#         name = request.POST.get("name")
+#         email = request.POST.get("email")
+#         message = request.POST.get("message")
+#
+#         # Добавляем сообщение
+#         print(f"Сообщение от {name} ({email}): {message}")
+#
+#     return render(request, "catalog/contacts.html", {"contact": contact_data})
 
 
-def add_product(request: HttpRequest) -> HttpResponse:
-    """
-    Обрабатывает добавление нового товара.
-    Обрабатывает ввод данных и сохраняет новый товар в базу данных.
-    """
-    if request.method == "POST":
-        form = ProductForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return redirect("catalog:home")  # Перевод на главную страницу после сохранения
-    else:
-        form = ProductForm()
+# def product_detail(request: HttpRequest, pk: int) -> HttpResponse:
+#     """Отображает детальную информацию о конкретном товаре."""
+#     product = Product.objects.get(pk=pk)
+#     context = {"object": product, "title": f"Купить {product.product_name}"}
+#     return render(request, "catalog/product_detail.html", context)
 
-    return render(request, "catalog/product_form.html", {"form": form})
+
+# def add_product(request: HttpRequest) -> HttpResponse:
+#     """
+#     Обрабатывает добавление нового товара.
+#     Обрабатывает ввод данных и сохраняет новый товар в базу данных.
+#     """
+#     if request.method == "POST":
+#         form = ProductForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             form.save()
+#             return redirect("catalog:home")  # Перевод на главную страницу после сохранения
+#     else:
+#         form = ProductForm()
+#
+#     return render(request, "catalog/product_form.html", {"form": form})
