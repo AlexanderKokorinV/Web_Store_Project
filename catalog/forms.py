84eed9452210bcea9c80998db45a7f8ada1f3a5f
Alexander <sanya.core@gmail.com>
2026-04-28 20:45:47 +0300

    form for roductProductForm has been updated

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,18 +1,82 @@
 from django import forms
-
+from django.core.exceptions import ValidationError
+from django.core.validators import FileExtensionValidator
 from .models import Product
 
 
 class ProductForm(forms.ModelForm):
     """Класс формы для добавления нового товара"""
 
+    forbidden_words = ["казино", "криптовалюта", "крипта", "биржа", "дешево","бесплатно", "обман", "полиция", "радар"]
+
     class Meta:
         model = Product
-        fields = ("product_name", "product_description", "image", "category_name", "price")
-        widgets = {
-            "product_name": forms.TextInput(attrs={"class": "form-control"}),
-            "product_description": forms.Textarea(attrs={"class": "form-control", "rows": 3}),
-            "image": forms.FileInput(attrs={"class": "form-control"}),
-            "category_name": forms.Select(attrs={"class": "form-control"}),
-            "price": forms.NumberInput(attrs={"class": "form-control"}),
-        }
+        fields = ["product_name", "product_description", "image", "category_name", "price"]
+
+    def __init__(self, *args, **kwargs):
+        super(ProductForm, self).__init__(*args, **kwargs)
+
+        self.fields["product_name"].widget.attrs.update({
+            "class": "form-control",
+            "placeholder": "Введите наименование продукта",
+        })
+
+        self.fields["product_description"].widget.attrs.update({
+            "class": "form-control",
+            "rows": 3,
+            "placeholder": "Введите описание продукта",
+        })
+
+        self.fields["image"].widget.attrs.update({
+            "class": "form-control",
+        })
+
+        self.fields["image"].validators.append(
+            FileExtensionValidator(
+                allowed_extensions=["jpeg", "jpg", "png"],
+                message="Неверный формат файла. Разрешены только JPEG, JPG и PNG.")
+        )
+
+        self.fields["category_name"].widget.attrs.update({
+            "class": "form-control",
+        })
+
+        self.fields["price"].widget.attrs.update({
+            "class": "form-control",
+        })
+
+    def clean_product_name(self):
+        cleaned_data = self.cleaned_data.get("product_name")
+
+        if cleaned_data:
+            for word in self.forbidden_words:
+                if word.lower() in cleaned_data.lower():
+                    raise ValidationError(f"Вы не можете использовать слово '{word}' в названии продукта")
+        return cleaned_data
+
+    def clean_product_description(self):
+        cleaned_data = self.cleaned_data.get("product_description")
+
+        if cleaned_data:
+            for word in self.forbidden_words:
+                if word.lower() in cleaned_data.lower():
+                    raise ValidationError(f"Вы не можете использовать слово '{word}' в описании продукта")
+        return cleaned_data
+
+    def clean_image(self):
+        image = self.cleaned_data.get("image")
+
+        if image:
+            max_size = 5*1024*1024
+            if image.size > max_size:
+                raise ValidationError("Размер файла не должен превышать 5 МБ.")
+
+        return image
+
+    def clean_price(self):
+        price = self.cleaned_data.get("price")
+
+        if price is not None and price < 0:
+            raise ValidationError("Цена продукта не может быть отрицательной")
+
+        return price
--- catalog/models.py
+++ catalog/models.py
@@ -7,18 +7,18 @@ class Product(models.Model):
     """Модель для хранения информации о продуктах"""
 
     product_name: models.CharField = models.CharField(
-        max_length=150, verbose_name="Наименование продукта", help_text="Введите наименование продукта"
+        max_length=150, verbose_name="Наименование продукта",
     )
     product_description: models.TextField = models.TextField(
-        verbose_name="Описание продукта", help_text="Введите описание продукта", blank=True, null=True
+        verbose_name="Описание продукта", blank=True, null=True
     )
     image: models.ImageField = models.ImageField(
         upload_to="catalog/image", blank=True, null=True, verbose_name="Изображение", help_text="Загрузите изображение"
     )
     category_name: models.ForeignKey = models.ForeignKey(
-        "Category", on_delete=models.CASCADE, verbose_name="Категория", related_name="products"
+        "Category", on_delete=models.CASCADE, verbose_name="Категория", help_text="Выберите категорию продукта", related_name="products"
     )
-    price: models.DecimalField = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Цена за покупку")
+    price: models.DecimalField = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Цена за покупку", help_text="Введите цену за шт.")
     created_at: models.DateTimeField = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
     updated_at: models.DateTimeField = models.DateTimeField(auto_now=True, verbose_name="Дата последнего изменения")
 
