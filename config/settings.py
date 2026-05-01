dbff3c97796f66d3e6fe9c31ed3fa6ba0a9db739
Alexander <sanya.core@gmail.com>
2026-05-01 21:31:15 +0300

    migrations have been set

--- config/settings.py
+++ config/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "catalog",
     "blog",
-    "users"
+    "users",
 ]
 
 MIDDLEWARE = [
@@ -135,3 +135,5 @@ MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+AUTH_USER_MODEL = "users.User"
--- users/models.py
+++ users/models.py
@@ -1,3 +1,47 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    username = None # Удаляем стандартное поле username
+    email = models.EmailField(
+        unique=True,
+        verbose_name="email"
+    )
+
+    phone_number = models.CharField(
+        max_length=15,
+        verbose_name="Номер телефона",
+        blank=True,
+        null=True,
+        help_text="Введите номер телефона"
+    )
+
+    avatar = models.ImageField(
+        upload_to="users/avatars/",
+        verbose_name="Аватар",
+        blank=True,
+        null=True,
+        help_text="Загрузите аватар"
+    )
+
+    country = models.CharField(
+        max_length=50,
+        verbose_name="Страна",
+        blank=True,
+        null=True,
+        help_text="Введите вашу страну"
+    )
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+
+    def __str__(self):
+        return self.email
+
+
+
