12cc4a787e6330fb3265e846a982aa246684b2ff
Alexander <sanya.core@gmail.com>
2026-05-02 14:47:23 +0300

    register view has been updated

--- config/settings.py
+++ config/settings.py
@@ -137,3 +137,16 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 AUTH_USER_MODEL = "users.User"
+
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/"
+
+EMAIL_HOST = "smtp.yandex.ru"
+EMAIL_PORT = 465
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
+
+SERVER_EMAIL = EMAIL_HOST_USER
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
--- users/views.py
+++ users/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from users.forms import UserRegisterForm
+from users.models import User
 
-# Create your views here.
+class UserRegisterView(CreateView):
+    model = User
+    form_class = UserRegisterForm
+    template_name = "users/register.html"
+    success_url = reverse_lazy("users:login")
+
+    def form_valid(self, form):
+        user = form.save() # Сохраняем пользователя
+        user.is_active = False
+
